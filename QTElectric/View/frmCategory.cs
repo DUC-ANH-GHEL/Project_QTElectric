b7e28e491355551a8c70918214c7ceacf2bdddb4
DUC-ANH-GHEL <aduc7246@gmail.com>
2021-03-30 17:59:26 +0700

    1

---
+++ QTElectric/View/frmCategory.cs
@@ -0,0 +1,42 @@
+﻿using QTElectric.DAO;
+using QTElectric.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QTElectric.View
+{
+    public partial class frmCategory : Form
+    {
+        public frmCategory()
+        {
+            InitializeComponent();
+            load();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Category c = new Category();
+            c.cat_name = txtCname.Text;
+            c.status = cbCstatus.Checked == true ? true : false;
+            c.date_ceate = DateTime.Now;
+            int result = CategoryDAO.Instance.InsertCategory(c);
+            if (result > 0)
+            {
+                MessageBox.Show("Insert Success");
+            }
+        }
+        public void load()
+        {
+            dvgCategory.DataSource = CategoryDAO.Instance.Categories();
+        }
+
+
+    }
+}
---
+++ QTElectric/frmLogin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QTElectric
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            //using (var stream = File.OpenRead(".\\Img\\logo.ico"))
+            //{
+            //    if (stream == null)
+            //    {
+            //        MessageBox.Show("Không tìm thấy");
+            //    }
+            //    else
+            //    {
+            //        this.Icon = new Icon(stream);
+            //    }
+
+            //}
+        }
+    }
+}
