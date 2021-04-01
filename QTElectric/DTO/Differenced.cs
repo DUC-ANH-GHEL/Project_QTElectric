e32b04985aac63ccde0e112d05bafec4af098c0d
DUC-ANH-GHEL <aduc7246@gmail.com>
2021-04-01 15:40:14 +0700

    update 1/4 3:40h

---
+++ QTElectric/DTO/Differenced.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTElectric.DTO
+{
+    public class Differenced
+    {
+        public int id { get; set; }
+        public string diff_name { get; set; }
+        public int val_id { get; set; }
+        public DateTime date_create { get; set; }
+        public bool status { get; set; }
+    }
+}
