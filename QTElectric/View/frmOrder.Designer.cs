92b3adea59ee68c35f6f8d1cab314dc47195170c
DUC-ANH-GHEL <aduc7246@gmail.com>
2021-04-01 21:36:34 +0700

    update 1/4 9:36pm

--- QTElectric/View/frmOrder.Designer.cs
+++ QTElectric/View/frmOrder.Designer.cs
@@ -51,9 +51,14 @@ namespace QTElectric.View
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -63,8 +68,9 @@ namespace QTElectric.View
             this.panel1.BackColor = System.Drawing.Color.Silver;
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(0, -2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(684, 56);
+            this.panel1.Size = new System.Drawing.Size(912, 69);
             this.panel1.TabIndex = 0;
             // 
             // label1
@@ -72,17 +78,19 @@ namespace QTElectric.View
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(208, 17);
+            this.label1.Location = new System.Drawing.Point(277, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(258, 24);
+            this.label1.Size = new System.Drawing.Size(339, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "NHẬP - KIỂM LINH KIỆN";
             // 
             // panel3
             // 
-            this.panel3.Location = new System.Drawing.Point(3, 58);
+            this.panel3.Location = new System.Drawing.Point(4, 71);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(675, 426);
+            this.panel3.Size = new System.Drawing.Size(900, 524);
             this.panel3.TabIndex = 1;
             // 
             // panel4
@@ -107,9 +115,10 @@ namespace QTElectric.View
             this.panel4.Controls.Add(this.label3);
             this.panel4.Controls.Add(this.label2);
             this.panel4.Controls.Add(this.pictureBox1);
-            this.panel4.Location = new System.Drawing.Point(0, 58);
+            this.panel4.Location = new System.Drawing.Point(0, 71);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(678, 435);
+            this.panel4.Size = new System.Drawing.Size(904, 535);
             this.panel4.TabIndex = 3;
             // 
             // button3
@@ -118,9 +127,10 @@ namespace QTElectric.View
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button3.Location = new System.Drawing.Point(455, 367);
+            this.button3.Location = new System.Drawing.Point(607, 452);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(81, 39);
+            this.button3.Size = new System.Drawing.Size(108, 48);
             this.button3.TabIndex = 9;
             this.button3.Text = "IN";
             this.button3.UseVisualStyleBackColor = false;
@@ -131,12 +141,14 @@ namespace QTElectric.View
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(278, 367);
+            this.button2.Location = new System.Drawing.Point(371, 452);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(81, 39);
+            this.button2.Size = new System.Drawing.Size(108, 48);
             this.button2.TabIndex = 8;
             this.button2.Text = "QR";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -144,9 +156,10 @@ namespace QTElectric.View
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(101, 367);
+            this.button1.Location = new System.Drawing.Point(135, 452);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(81, 39);
+            this.button1.Size = new System.Drawing.Size(108, 48);
             this.button1.TabIndex = 7;
             this.button1.Text = "FILE";
             this.button1.UseVisualStyleBackColor = false;
@@ -156,66 +169,76 @@ namespace QTElectric.View
             this.panel5.BackColor = System.Drawing.Color.White;
             this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel5.Location = new System.Drawing.Point(36, 230);
+            this.panel5.Controls.Add(this.pictureBox3);
+            this.panel5.Controls.Add(this.pictureBox2);
+            this.panel5.Location = new System.Drawing.Point(48, 283);
+            this.panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(447, 100);
+            this.panel5.Size = new System.Drawing.Size(595, 123);
             this.panel5.TabIndex = 4;
             // 
             // cbxDiff
             // 
             this.cbxDiff.FormattingEnabled = true;
-            this.cbxDiff.Location = new System.Drawing.Point(525, 308);
+            this.cbxDiff.Location = new System.Drawing.Point(700, 379);
+            this.cbxDiff.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbxDiff.Name = "cbxDiff";
-            this.cbxDiff.Size = new System.Drawing.Size(121, 21);
+            this.cbxDiff.Size = new System.Drawing.Size(160, 24);
             this.cbxDiff.TabIndex = 6;
             // 
             // comboBox5
             // 
             this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(525, 245);
+            this.comboBox5.Location = new System.Drawing.Point(700, 302);
+            this.comboBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.Size = new System.Drawing.Size(160, 24);
             this.comboBox5.TabIndex = 5;
             // 
             // cbxValue
             // 
             this.cbxValue.FormattingEnabled = true;
-            this.cbxValue.Location = new System.Drawing.Point(525, 182);
+            this.cbxValue.Location = new System.Drawing.Point(700, 224);
+            this.cbxValue.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbxValue.Name = "cbxValue";
-            this.cbxValue.Size = new System.Drawing.Size(121, 21);
+            this.cbxValue.Size = new System.Drawing.Size(160, 24);
             this.cbxValue.TabIndex = 4;
             // 
             // cbxType
             // 
             this.cbxType.FormattingEnabled = true;
-            this.cbxType.Location = new System.Drawing.Point(362, 182);
+            this.cbxType.Location = new System.Drawing.Point(483, 224);
+            this.cbxType.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbxType.Name = "cbxType";
-            this.cbxType.Size = new System.Drawing.Size(121, 21);
+            this.cbxType.Size = new System.Drawing.Size(160, 24);
             this.cbxType.TabIndex = 3;
             // 
             // cbxCat
             // 
             this.cbxCat.FormattingEnabled = true;
-            this.cbxCat.Location = new System.Drawing.Point(199, 182);
+            this.cbxCat.Location = new System.Drawing.Point(265, 224);
+            this.cbxCat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbxCat.Name = "cbxCat";
-            this.cbxCat.Size = new System.Drawing.Size(121, 21);
+            this.cbxCat.Size = new System.Drawing.Size(160, 24);
             this.cbxCat.TabIndex = 2;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(36, 182);
+            this.comboBox1.Location = new System.Drawing.Point(48, 224);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
             this.comboBox1.TabIndex = 1;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(544, 284);
+            this.label8.Location = new System.Drawing.Point(725, 350);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(77, 21);
+            this.label8.Size = new System.Drawing.Size(94, 27);
             this.label8.TabIndex = 2;
             this.label8.Text = "Sai số %";
             // 
@@ -223,9 +246,10 @@ namespace QTElectric.View
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(544, 221);
+            this.label7.Location = new System.Drawing.Point(725, 272);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(78, 21);
+            this.label7.Size = new System.Drawing.Size(97, 27);
             this.label7.TabIndex = 2;
             this.label7.Text = "Số lượng";
             // 
@@ -233,9 +257,10 @@ namespace QTElectric.View
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(543, 158);
+            this.label6.Location = new System.Drawing.Point(724, 194);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(95, 21);
+            this.label6.Size = new System.Drawing.Size(123, 27);
             this.label6.TabIndex = 2;
             this.label6.Text = "Tên/ Giá trị";
             // 
@@ -243,9 +268,10 @@ namespace QTElectric.View
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(380, 158);
+            this.label5.Location = new System.Drawing.Point(507, 194);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(84, 21);
+            this.label5.Size = new System.Drawing.Size(110, 27);
             this.label5.TabIndex = 2;
             this.label5.Text = "Kiểu chân";
             // 
@@ -253,9 +279,10 @@ namespace QTElectric.View
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(205, 158);
+            this.label4.Location = new System.Drawing.Point(273, 194);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(109, 21);
+            this.label4.Size = new System.Drawing.Size(145, 27);
             this.label4.TabIndex = 2;
             this.label4.Text = "Loại linh kiện";
             // 
@@ -263,9 +290,10 @@ namespace QTElectric.View
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(54, 158);
+            this.label3.Location = new System.Drawing.Point(72, 194);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 21);
+            this.label3.Size = new System.Drawing.Size(104, 27);
             this.label3.TabIndex = 2;
             this.label3.Text = "STT Bom";
             // 
@@ -273,9 +301,10 @@ namespace QTElectric.View
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(475, 31);
+            this.label2.Location = new System.Drawing.Point(633, 38);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(176, 46);
+            this.label2.Size = new System.Drawing.Size(226, 62);
             this.label2.TabIndex = 1;
             this.label2.Text = "NGÀY NHẬP\r\n10:20 - 11/12/2020\r\n";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -283,21 +312,41 @@ namespace QTElectric.View
             // pictureBox1
             // 
             this.pictureBox1.Image = global::QTElectric.Properties.Resources.logo;
-            this.pictureBox1.Location = new System.Drawing.Point(36, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(48, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(168, 128);
+            this.pictureBox1.Size = new System.Drawing.Size(224, 158);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(199, 116);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(274, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(316, 79);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
             // frmOrder
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LemonChiffon;
-            this.ClientSize = new System.Drawing.Size(679, 485);
+            this.ClientSize = new System.Drawing.Size(905, 597);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel3);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmOrder";
             this.Text = "NHẬP - KIỂM LINH KIỆN";
             this.Load += new System.EventHandler(this.frmOrder_Load_1);
@@ -305,7 +354,10 @@ namespace QTElectric.View
             this.panel1.PerformLayout();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -334,5 +386,7 @@ namespace QTElectric.View
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
