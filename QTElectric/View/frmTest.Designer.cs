2c75ce2e7c93d69835196ec8507c5d508c32c1c1
minh200x <67300487+minh200x@users.noreply.github.com>
2021-04-15 17:51:29 +0700

    cmt form

---
+++ QTElectric/View/frmTest.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace QTElectric.View
+{
+    partial class frmTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvOrder = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrder)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvOrder
+            // 
+            this.dgvOrder.AllowUserToAddRows = false;
+            this.dgvOrder.AllowUserToDeleteRows = false;
+            this.dgvOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrder.Location = new System.Drawing.Point(12, 36);
+            this.dgvOrder.Name = "dgvOrder";
+            this.dgvOrder.ReadOnly = true;
+            this.dgvOrder.Size = new System.Drawing.Size(360, 206);
+            this.dgvOrder.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.AutoSize = true;
+            this.txtName.Location = new System.Drawing.Point(126, 9);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(33, 13);
+            this.txtName.TabIndex = 4;
+            this.txtName.Text = "name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tên khách hàng";
+            // 
+            // frmTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 259);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvOrder);
+            this.Name = "frmTest";
+            this.Text = "frmTest";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrder)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvOrder;
+        private System.Windows.Forms.Label txtName;
+        private System.Windows.Forms.Label label1;
+    }
+}
